b29e52d5a951d7f98adfbf78cce75c1885b36645
LeBwa <lebwa1337@gmail.com>
2024-04-05 16:52:53 +0300

    Add authentication endpoint

--- book_library/urls.py
+++ book_library/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("books.urls", namespace="books"))
+    path("api/", include("books.urls", namespace="books")),
+    path('api-auth/', include('rest_framework.urls'))
 ]
