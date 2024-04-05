8cbec9f49aef9a852c3cff10afbc95ee81a60205
LeBwa <lebwa1337@gmail.com>
2024-04-06 00:13:12 +0300

    Implement Borrowing model and run migration

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                name="unique_date",
+                fields=[
+                    "borrow_date",
+                    "expected_return_date",
+                    "actual_return_date"
+                ],
+            )
+        ]
+
+    def __str__(self):
+        return self.book
