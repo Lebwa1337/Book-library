69361f117ed36b592226fa306c4c2cce08205314
LeBwa <lebwa1337@gmail.com>
2024-04-05 17:33:55 +0300

    Create user model and specify it in settings

--- book_library/settings.py
+++ book_library/settings.py
@@ -137,3 +137,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    """User model."""
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
