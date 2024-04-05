c630ace8ade626b5ec27390bb28e37707a05e9dc
LeBwa <lebwa1337@gmail.com>
2024-04-05 15:45:40 +0300

    Create Book model and run migration

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class BookCover(models.TextChoices):
+        SOFT = "SF", _("Soft")
+        HARD = "HD", _("Hard")
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    cover = models.CharField(
+        max_length=2,
+        choices=BookCover.choices
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
