8696a1f936daaf007ed34c5111b607657d3bf0ab
LeBwa <lebwa1337@gmail.com>
2024-04-05 18:14:21 +0300

    Add path to user app and move token endpoint to user app urls

--- book_library/urls.py
+++ book_library/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/user/", include("users.urls", namespace="users")),
 ]
