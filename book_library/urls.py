27e6a42f457e73a68116632b32809fc5ee9a7bb0
LeBwa <lebwa1337@gmail.com>
2024-04-05 18:21:13 +0300

    Change endpoint from user to users

--- book_library/urls.py
+++ book_library/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
-    path("api/user/", include("users.urls", namespace="users")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
