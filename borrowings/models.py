cb58cbed4fc24071a9f7b497fc2f97a99fd8f9c0
LeBwa <lebwa1337@gmail.com>
2024-04-06 13:54:19 +0300

    Create validation for borrow creation

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import UniqueConstraint
 
 from books.models import Book
 from users.models import User
@@ -12,17 +12,19 @@ class Borrowing(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        constraints = [
-            UniqueConstraint(
-                name="unique_date",
-                fields=[
-                    "borrow_date",
-                    "expected_return_date",
-                    "actual_return_date"
-                ],
-            )
-        ]
-
     def __str__(self):
         return self.book
+
+    @staticmethod
+    def validate_dates(borrow_date, expected_return_date):
+        if borrow_date > expected_return_date:
+            raise ValidationError("Borrow date should be less than expected or actual return date")
+
+    def clean(self):
+        Borrowing.validate_dates(self.borrow_date, self.expected_return_date)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(force_insert, force_update, using, update_fields)
