efb24a72d85f89ccd315b36e66e8fb7e3bac5868
LeBwa <lebwa1337@gmail.com>
2024-04-11 19:55:12 +0300

    Set access token to 7 days for comfortable developing

--- book_library/settings.py
+++ book_library/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
