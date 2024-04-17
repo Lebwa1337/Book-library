d15b2f95b174ede697d6d1aace18ad411b02c33c
LeBwa <lebwa1337@gmail.com>
2024-04-17 18:42:29 +0300

    Install and add endpoints for swagger documentation

--- book_library/urls.py
+++ book_library/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
-    path("api/", include("borrowings.urls", namespace="borrowings"))
+    path("api/", include("borrowings.urls", namespace="borrowings")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/doc/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
 ]
